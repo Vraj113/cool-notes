7a93e69af6fdbe9f6acc0111722e2fc20c23f2db
Vraj Roy <vrajroy2k11@gmail.com>
2024-09-19 19:08:35 +0530

    pwa fixes 3 import aded

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
-
-const withPWA = require("next-pwa")({
-  dest: "public", // Folder where service worker is generated
-  register: true, // Registers service worker automatically
-  skipWaiting: true, // Installs updates automatically
-});
-
 const nextConfig = {
   reactStrictMode: true,
   // You can add other Next.js config options here
 };
 
-module.exports = withPWA(nextConfig);
+const pwaConfig = {
+  dest: "public", // Folder where service worker is generated
+  register: true, // Registers service worker automatically
+  skipWaiting: true, // Installs updates automatically
+};
+
+export default withPWA(pwaConfig)(nextConfig);

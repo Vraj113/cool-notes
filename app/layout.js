bc2666c9ca297f4045a7ee8c4e619d37c4f919d7
Vraj Roy <vrajroy2k11@gmail.com>
2024-09-19 18:51:46 +0530

    pwa fixes

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,19 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import AuthProvider from "@/auth/AuthProvider";
-import Head from "next/head";
 
 export const metadata = {
   title: "Cute Notes App",
   description: "❤️❤️❤️",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-      </Head>
       <AuthProvider>
-        <body className={` font-sans antialiased `}>{children}</body>
+        <body className={`font-sans antialiased`}>{children}</body>
       </AuthProvider>
     </html>
   );

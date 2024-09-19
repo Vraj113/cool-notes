8870a64dad5eaeaf0c7e13fbf0b5fe2859107cd2
Vraj Roy <vrajroy2k11@gmail.com>
2024-09-19 18:45:58 +0530

    pwa added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import AuthProvider from "@/auth/AuthProvider";
+import Head from "next/head";
 
 export const metadata = {
   title: "Cute Notes App",
@@ -10,6 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <AuthProvider>
         <body className={` font-sans antialiased `}>{children}</body>
       </AuthProvider>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public", // The folder where service worker and other PWA assets will be generated
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);

dd332eaef27d241c0e923fb0d35c2c2b987c9520
Vraj Roy <vrajroy2k11@gmail.com>
2024-09-23 20:11:47 +0530

    middleware fix

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ export async function middleware(request) {
   const cookieStore = cookies();
   const token = cookieStore.get("next-auth.session-token")?.value;
   const { pathname } = request.nextUrl;
-
+  console.log(token + "pathname: " + pathname);
   // Allow access to the login page without a token
   if (pathname === "/login") {
     if (token) {

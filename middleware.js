6e9dc4d3c6a084f23f88ae2d80655d5d33782f67
Vraj Roy <vrajroy2k11@gmail.com>
2024-09-24 23:53:18 +0530

    .

--- middleware.js
+++ middleware.js
@@ -1,13 +1,20 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { getToken } from "next-auth/jwt";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "../notes/app/api/auth/[...nextauth]/route";
 
 export async function middleware(request) {
   const cookieStore = cookies();
+  // const session = await getServerSession(authOptions);
+  // console.log(session);
   const token =
     cookieStore.get("next-auth.session-token")?.value ||
     cookieStore.get("__Secure-next-auth.session-token")?.value;
   const { pathname } = request.nextUrl;
   // Allow access to the login page without a token
+
   if (pathname === "/login") {
     if (token) {
       // If logged in, redirect away from login page to home
